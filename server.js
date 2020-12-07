bd8d01f9d7e1aceb2a5b0680c5a351e01d0de6cf
Joshua Pribek <joshpribek@icloud.com>
2020-12-06 20:10:10 -0800

    deploying to herkou

--- server.js
+++ server.js
@@ -1,17 +1,55 @@
 const express = require("express");
 const fs = require("fs");
-const path = require("path");
 const app = express();
+const port = process.env.port || 3005;
 
-const PORT = process.env.PORT || 8010;
-
-app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
-require("/routes/apiroutes")(app);
-require("/routes/htmlroutes")(app);
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/api/notes/", function (req, res) {
+  fs.readFile(__dirname + "/db/db.json", (err, data) => {
+    var json = JSON.parse(data);
+    return res.json(json);
+  });
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/api/notes/", function (req, res) {
+  newNote = req.body;
+
+  fs.readFile(__dirname + "/db/db.json", (err, data) => {
+    var json = JSON.parse(data);
+    json.push(newNote);
+
+    fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(json));
+  });
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+  let response = req.params;
+  let id = response.id;
+  console.log(`Note id: ${id} marked for deletion`);
+
+  fs.readFile(__dirname + "/db/db.json", (err, data) => {
+    var json = JSON.parse(data);
+
+    const filteredJson = json.filter((element) => element.id !== id);
+    fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(filteredJson));
+  });
+});
+
+app.listen(process.env.PORT || 3005, () => {
+  console.log(`Started server`);
 });
