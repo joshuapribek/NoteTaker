1b89dbd06ad6e5c875f59a2a894cefcd78ec3514
Joshua Pribek <joshpribek@icloud.com>
2020-12-07 17:25:36 -0800

    finished

--- server.js
+++ server.js
@@ -2,17 +2,19 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 const path = require("path");
-const port = process.env.port || 3005;
+const uuid = require("uuid");
+const port = process.env.PORT || 3005;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname + "/notes.html"));
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.get("/api/notes/", function (req, res) {
@@ -23,17 +25,19 @@ app.get("/api/notes/", function (req, res) {
 });
 
 app.get("*", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname, "./public/index.html");
 });
 
 app.post("/api/notes/", function (req, res) {
   newNote = req.body;
-
+  newNote.id = uuid.v4()
   fs.readFile(__dirname + "/db/db.json", (err, data) => {
     var json = JSON.parse(data);
     json.push(newNote);
 
     fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(json));
+
+    res.json(true)
   });
 });
 
@@ -47,6 +51,8 @@ app.delete("/api/notes/:id", function (req, res) {
 
     const filteredJson = json.filter((element) => element.id !== id);
     fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(filteredJson));
+
+    res.json(true)
   });
 });
 
